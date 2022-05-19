582a5ad900a4bf4678e199bf62db51dfc87ed165
Topa592 <47034446+Topa592@users.noreply.github.com>
2022-05-19 12:50:03 +0300

    renamed files to match upcoming features better

---
+++ tl/tlBasicCodeEditor.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+// Code editor to copy text into another file 
+//
+// you need to specify the area your text is copyed into comments
+//
+// Example for Autohotkey code
+// ;myCopyPasteStart
+// ...
+// ;myCopyPasteEnd
+//
+// ... stands for there could be multiple lines of the copied code
+//
+// 
