ff4597218cb7d01609d01eb023d7ee68e3264a7b
Topa592 <47034446+Topa592@users.noreply.github.com>
2023-08-28 02:19:15 +0300

    Added FolderPaths

--- tl/tlFilesystem.h
+++ tl/tlFilesystem.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <filesystem>
 #include <string>
+#include <vector>
 
 namespace tl {
 	namespace filesystem {
@@ -28,5 +29,41 @@ namespace tl {
 		std::string RelativeToDirectPath(const std::string& file) {
 			return ExeFolderPath() + '\\' + file;
 		}
+		struct FolderPaths {
+			std::vector<std::filesystem::path> files;
+			void sortByDate(bool oldestFirst = true) {
+				std::vector<std::filesystem::path> sortedByDate;
+				for (const auto& path : files) {
+					bool notAdded = true;
+					std::filesystem::file_time_type curPathTime = std::filesystem::last_write_time(path);
+					for (int i = 0; i < sortedByDate.size(); i++) {
+						std::filesystem::file_time_type time = std::filesystem::last_write_time(sortedByDate[i]);
+						if (oldestFirst) {
+							if (curPathTime < time) continue;
+						}
+						else {
+							if (curPathTime > time) continue;
+						}
+						sortedByDate.insert(std::next(sortedByDate.begin(), i), path);
+						notAdded = false;
+						break;
+					}
+					if (notAdded) {
+						sortedByDate.push_back(path);
+					}
+				}
+				files = sortedByDate;
+			}
+			void addFolderFiles(std::filesystem::path folder, std::string optionalAddOnlyFileExtension = "") {
+				for (auto& p : std::filesystem::directory_iterator(folder)) {
+					auto extension = p.path().extension();
+					std::string s = extension.generic_string();
+					if (!optionalAddOnlyFileExtension.empty() && !s.ends_with(optionalAddOnlyFileExtension)) {
+						continue;
+					}
+					files.push_back(p);
+				}
+			}
+		};
 	}
 }
